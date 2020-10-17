271eb69313bfc69fadd5a4c693240a3d274c8322
SANDEEP-139 <139sandeepsingh@gmail.com>
2020-10-17 10:38:47 -0700

    blog commit

---
+++ resources/views/list.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel 6.0 CRUD Application</title>
+
+        <!-- Fonts -->
+         <link rel="stylesheet" href="{{asset('blog/public/css/bootstrap.css')}}">
+      
+</head>
+<body class="bg-light">
+	
+</body>
+</html>
